9f6f1b20b77d329f58834fe85a2b189c76a1d18f
Nostrum <daniel.soler@e-campus.uab.cat>
2020-03-19 03:35:47 +0100

    Give style to form via cryspi and html

--- ML_NBD/generative_model/forms.py
+++ ML_NBD/generative_model/forms.py
@@ -5,3 +5,14 @@ class GenerativeForm(forms.ModelForm):
     class Meta:
         model = md.GenerativeModel
         fields = ('pdb', 'residue_name', 'iterations')
+        widgets = {
+            'pdb': forms.FileInput( attrs={'class': 'pdbclass'}),
+            'residue_name': forms.TextInput(attrs={'class': 'residueclass'}),
+            'iterations': forms.NumberInput(attrs={'class': 'iterationsclass'})
+
+        }
+        labels = {
+            "pdb": "Ligand pdb:",
+            "residue_name": "Ligand residue name:",
+            "iterations": "Growing Iterations"
+        }
